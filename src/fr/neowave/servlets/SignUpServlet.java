9620ccf1b4b5c5535c2d956afb7414bd3ddcb8e8
Thomas Margier <thomas.margier@live.fr>
2015-10-18 20:16:22 +0200

    looool

--- src/fr/neowave/servlets/SignUpServlet.java
+++ src/fr/neowave/servlets/SignUpServlet.java
@@ -1,6 +1,5 @@
 package fr.neowave.servlets;
 
-import fr.neowave.beans.User;
 import fr.neowave.forms.RegistrationForm;
 
 import javax.servlet.ServletException;
@@ -10,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
 
 @WebServlet("/signUp")
@@ -20,39 +22,67 @@ public class SignUpServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        if(request.getSession().getAttribute("username") == null ||  request.getSession().getAttribute("username").equals("admin") ){
-            this.getServletContext().getRequestDispatcher("/WEB-INF/user/signUp.jsp").forward(request, response);
-        }
-        else{
-            response.sendRedirect(request.getContextPath().concat("/index"));
-        }
+        response.addHeader("X-XSS-Protection", "1; mode=block");
+        response.addHeader("X-Frame-Options", "DENY; SAMEORIGIN");
+        response.addHeader("X-Content-Type-Options", "nosniff");
+        response.addHeader("Content-Security-Policy", "img-src 'self';" +
+                "media-src 'self';font-src 'self'");
+        URLConnection connection = new URL(request.getRequestURL().toString()).openConnection();
+        List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+
+        if (cookies != null)
+            for(String cookie : cookies){
+                response.setHeader("Set-Cookie", cookie.concat("; HttpOnly;"));
+            }
+
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/user/signUp.jsp").forward(request, response);
 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        response.addHeader("X-XSS-Protection", "1; mode=block");
+        response.addHeader("X-Frame-Options", "DENY; SAMEORIGIN");
+        response.addHeader("X-Content-Type-Options", "nosniff");
+        response.addHeader("Content-Security-Policy", "img-src 'self';" +
+                "media-src 'self';font-src 'self'");
+        URLConnection connection = new URL(request.getRequestURL().toString()).openConnection();
+        List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
 
-        RegistrationForm registrationForm = new RegistrationForm();
-        User user = registrationForm.register(request);
-
-        HttpSession session = request.getSession();
-
-        if(registrationForm.getErrors().isEmpty()){
-
-            if(request.getSession().getAttribute("username") != null || !request.getSession().getAttribute("username").equals("admin")){
-                response.sendRedirect(request.getContextPath().concat("/index"));
+        if (cookies != null)
+            for(String cookie : cookies){
+                response.setHeader("Set-Cookie", cookie.concat("; HttpOnly;"));
             }
-            else{
-                session.setAttribute("username", user.getUsername());
-                session.setMaxInactiveInterval(86400000);
-                response.sendRedirect(request.getContextPath().concat("/index"));
+        if(request.getSession().getAttribute("username") == null || request.getSession().getAttribute("username").equals("admin")) {
+
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.register(request);
+
+            HttpSession session = request.getSession();
+
+            if (registrationForm.getErrors().isEmpty()) {
+                if(session.getAttribute("username").equals("admin")){
+
+                    response.sendRedirect(request.getContextPath().concat("/adminManage"));
+                }
+                else{
+
+                    response.sendRedirect(request.getContextPath().concat("/index"));
+                }
+
+
+            } else {
+                if(session.getAttribute("username") != null && !session.getAttribute("username").equals("admin")) session.setAttribute("username", null);
+                request.setAttribute("username", request.getParameter("username"));
+                request.setAttribute("errors", registrationForm.getErrors());
+
+                this.getServletContext().getRequestDispatcher("/WEB-INF/user/signUp.jsp").forward(request, response);
             }
 
         }
         else{
-            session.setAttribute("username", null);
-            request.setAttribute("username", request.getParameter("username"));
-            request.setAttribute("error", registrationForm.getErrors());
-            this.getServletContext().getRequestDispatcher("/WEB-INF/user/signUp.jsp").forward(request, response);
+            response.sendRedirect(request.getContextPath().concat("/signUp"));
         }
+
     }
 }
